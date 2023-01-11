912fa6570ee06d87c35342875ba189af9b899944
lacattano <louis.cattano@lv.co.uk>
2023-01-11 12:14:21 +0000

    changed l29 in scheduled jobs

--- scheduled_jobs.py
+++ scheduled_jobs.py
@@ -26,14 +26,14 @@ def process_orders(app):
         payload = {
             "product": order.product,
             "customer": order.customer,
-            "date": order.date_placed.isoformat(),
+            "date": order.date_placed_local.isoformat(),
         }
 
         response = requests.post(
             app.config["FINANCE_PACKAGE_URL"] + "/ProcessPayment",
             json=payload
         )
-
+        app.logger.info("Response from endpoint: " + response.text)
         response.raise_for_status()
 
         order.set_as_processed()
